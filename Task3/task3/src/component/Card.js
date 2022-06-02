38ef6716e1154b4fefc732ee373c0e6142c041f4
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-06-02 20:02:06 +0600

    task 3 complete

--- Task3/task3/src/component/Card.js
+++ Task3/task3/src/component/Card.js
@@ -6,21 +6,23 @@ import switch1 from "../img/switch.png";
 
 const Card = () => {
   const [channel, setChannel] = useState([]);
-  const [flag, setFlag] = useState(null);
+  const [imgIndex, setImgIndex] = useState(null);
+
+  const [fm, setFm] = useState("");
   useEffect(() => {
     fetch("fake.json")
       .then((res) => res.json())
       .then((data) => setChannel(data));
-  }, []);
+  }, [fm]);
   console.log(channel);
   return (
-    <div className="mx-auto  w-[380px] h-[600px]  shadow-2xl overflow-hidden rounded-[60px] flex flex-col justify-between">
+    <div className="mx-auto  w-[380px] h-[600px]  shadow-2xl overflow-hidden rounded-[60px] flex flex-col justify-between bg-[#2a2a35]">
       <div>
-        <div className="bg-[#eeae61] py-7 flex justify-around items-center">
-          <button>
+        <div className="bg-[#eeae61] py-7 flex h-[100px] justify-around items-center">
+          <button className=" bg-transparent  border-0">
             <img style={{ width: "18px" }} src={back} alt="" />
           </button>
-          <h2 className=" font-sans text-3xl font-bold  text-white">
+          <h2 className=" font-mono text-3xl font-bold  text-white">
             STATIONS
           </h2>
           <button>
@@ -29,13 +31,26 @@ const Card = () => {
         </div>
         <div className="py-2 overflow-y-scroll h-[400px] scrollbar-hide ">
           {channel?.map((fm, index) => (
-            <FmChannel key={index} fm={fm}></FmChannel>
+            <FmChannel
+              key={index}
+              index={index}
+              fm={fm}
+              setFm={setFm}
+              setImgIndex={setImgIndex}
+              imgIndex={imgIndex}
+            ></FmChannel>
           ))}
         </div>
       </div>
-      <div className="bg-[#22222b] py-7">
-        <h2 className="text-[#eeae61] text-sm font-bold">CURRENTLY PLAYING</h2>
-        <h2 className=" font-sans text-2xl    ">Booth Fm</h2>
+      <div className="bg-[#22222b] py-7 h-[100px]">
+        {fm && (
+          <>
+            <h2 className="text-[#eeae61] text-xs font-bold font-mono">
+              CURRENTLY PLAYING
+            </h2>
+            <h2 className=" font-sans text-2xl    ">{fm.name}</h2>
+          </>
+        )}
       </div>
     </div>
   );
